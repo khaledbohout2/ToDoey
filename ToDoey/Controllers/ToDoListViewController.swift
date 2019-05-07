0ec4ef6ade5c7179daab1df419ef277b5076395e
Khaled Bohout <khaledbohout2012@gmail.com>
2019-05-07 16:35:49 +0200

    persisting data using coddle protocol

--- ToDoey/Controllers/ToDoListViewController.swift
+++ ToDoey/Controllers/ToDoListViewController.swift
@@ -10,40 +10,25 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArr = [Item]()
+    var itemArray = [Item]()
+    let dataFliePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-    let defaults = UserDefaults.standard
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let newitem = Item()
-        newitem.title = "Learn DS"
-        itemArr.append(newitem)
-        
-        let newitem2 = Item()
-        newitem2.title = "Learn DS"
-        itemArr.append(newitem2)
-        
-        let newitem3 = Item()
-        newitem3.title = "Learn DS"
-        itemArr.append(newitem3)
-        
-        
-        //defaults.removeObject(forKey: "itemArr")
-        if let items = defaults.array(forKey: "itemArr") as? [Item] {
-            itemArr =  items
-        }
+    
+        loaditems()
         // Do any additional setup after loading the view.
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArr.count
+        return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListItemCell", for: indexPath)
-        let item = itemArr[indexPath.row]
+        let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
         
         if item.done == true {
@@ -57,12 +42,12 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let item = itemArr[indexPath.row]
+        let item = itemArray[indexPath.row]
         item.done = !(item.done)
         print(item.done)
     
          
-         tableView.reloadData()
+         self.saveItem()
         
     }
 
@@ -73,11 +58,14 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //tap add item
             print("success")
+            
             let newitem = Item()
+            
             newitem.title = textfield.text!
-            self.itemArr.append(newitem)
-            self.defaults.setValue(self.itemArr, forKey: "itemArr")
-            self.tableView.reloadData()
+            
+            self.itemArray.append(newitem)
+            
+            self.saveItem()
         }
         alert.addAction(action)
         alert.addTextField { (alerttextfield) in
@@ -87,6 +75,32 @@ class ToDoListViewController: UITableViewController {
             
         }
         present(alert, animated: true)
+    }
+    func saveItem(){
+        
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFliePath!)
+            
+        } catch{
+            print("error encoding item array, \(error)")
+        }
+        self.tableView.reloadData()
+        
+    }
+    
+    func loaditems(){
+        
+        if let data = try? Data(contentsOf: dataFliePath!){
+            let decoder = PropertyListDecoder()
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+            }
+            catch{
+                print("error decoding array , \(error)")
+            }
+        }
     }
     
 }
